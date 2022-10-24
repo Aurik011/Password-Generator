fdd30ab1c425a24e094669a3290c2d9a62d16f8a
Aurik Anjum <109861996+Aurik011@users.noreply.github.com>
2022-10-24 14:51:36 +0600

    Second commit password input twice

--- password_generator.py
+++ password_generator.py
@@ -18,3 +18,22 @@ for password in range(amount):
         passwords += random.choice(character)
     print(passwords)
 
+prompt = input("Do you want to generate more passwords? ") # prompt for more
+if prompt.lower() != "yes":
+    quit() # quits the program
+
+print("Okay, Generating passwords for the second time: ")
+
+amount = input("Amount of passwords that we will generate:  ")
+amount = int(amount) # must be an integer
+
+length = input("Length of the passwords:  ")
+length = int(length) # must be an integer
+
+print("Generated Passwords:")
+
+for password in range(amount):
+    passwords = ""
+    for c in range(length):
+        passwords += random.choice(character)
+    print(passwords)
