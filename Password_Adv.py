442481149bef3049d5c6598f566088ec356da2df
Aurik Anjum <109861996+Aurik011@users.noreply.github.com>
2022-10-28 21:33:48 +0600

    Three methods of password choice

---
+++ Password_Adv.py
@@ -0,0 +1,67 @@
+import random
+
+print("WELCOME TO THE SIMPLE PASSWORD GENERATOR TOOL")
+
+character="QWERTYUIOPqwertyuiopASDFGHJKLasdfghjklZXCVBNMzxcvbnm1234567890!@#$%^&*()"
+# SYMBOLS AND NUMBERS ONLY PASSWORDS 1234567890!@#$%^&*()
+# ALPHABETS ONLY PASSWORDS QWERTYUIOP qwertyuiopASDFGHJKLasdfghjklZXCVBNMzxcvbnm
+
+amount = input("Amount of passwords that we will generate:  ")
+amount = int(amount) # must be an integer
+
+length = input("Length of the passwords:  ")
+length = int(length) # must be an integer
+
+print("Generated Passwords:")
+
+for password in range(amount):
+    passwords = ""
+    for c in range(length):
+        passwords += random.choice(character)
+    print(passwords)
+
+prompt = input("Do you want to generate character only passwords? ") # prompt for more
+if prompt.lower() != "yes":
+    quit() # quits the program
+
+character="QWERTYUIOPqwertyuiop"
+
+print("Okay, Generating passwords for the second time: ")
+print("Character only passwords: ")
+
+amount = input("Amount of passwords that we will generate:  ")
+amount = int(amount) # must be an integer
+
+length = input("Length of the passwords:  ")
+length = int(length) # must be an integer
+
+print("Generated Passwords:")
+
+for password in range(amount):
+    passwords = ""
+    for c in range(length):
+        passwords += random.choice(character)
+    print(passwords)
+
+prompt = input("Do you want to number and symbols only passwords? ") # prompt for more
+if prompt.lower() != "yes":
+    quit() # quits the program
+
+character="1234567890!@#$%^&*()"
+
+print("Okay, Generating passwords for the second time: ")
+print("Character only passwords: ")
+
+amount = input("Amount of passwords that we will generate:  ")
+amount = int(amount) # must be an integer
+
+length = input("Length of the passwords:  ")
+length = int(length) # must be an integer
+
+print("Generated Passwords:")
+
+for password in range(amount):
+    passwords = ""
+    for c in range(length):
+        passwords += random.choice(character)
+    print(passwords)
